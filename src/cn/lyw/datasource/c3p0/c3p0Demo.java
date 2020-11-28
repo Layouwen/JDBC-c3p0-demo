ae918c2c94c200dc9afd5b010061d1b341c8ac95
liangyouwen <layouwen@gmail.com>
2020-11-28 17:39:51 +0800

    c3d0Demo1: 简单完成连接从c3p0

---
+++ src/cn/lyw/datasource/c3p0/c3p0Demo.java
@@ -0,0 +1,18 @@
+package cn.lyw.datasource.c3p0;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class c3p0Demo {
+  public static void main(String[] args) throws SQLException {
+    // 创建数据库连接对象
+    DataSource ds = new ComboPooledDataSource();
+    // 获取连接对象
+    Connection conn = ds.getConnection();
+
+    System.out.println(conn);
+  }
+}
